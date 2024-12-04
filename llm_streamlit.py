8465a4ea3e69aa8b006dba5316597ebcc0d825f1
Nour19am <133000818+Nour19am@users.noreply.github.com>
2024-12-04 12:25:04 +0100

    Update llm_streamlit.py

--- llm_streamlit.py
+++ llm_streamlit.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 
 client = OpenAI(
   base_url = "https://integrate.api.nvidia.com/v1",
-  api_key = "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"
+  api_key = "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"
 )
 
 st.title("Agent financier")
@@ -12,9 +12,9 @@ st.title("Agent financier")
 # Set OpenAI API key from Streamlit secrets
 #client=ChatNVIDIA(api_key="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",model="meta/llama3-70b-instruct")
 #client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-from langchain_nvidia_ai_endpoints import ChatNVIDIA
 
-model = ChatNVIDIA(model="nvidia/nemotron-4-340b-instruct")
+
+#model = ChatNVIDIA(model="nvidia/nemotron-4-340b-instruct")
 
 if "nvidia_model" not in st.session_state:
     #st.session_state["nvidia_model"] = "meta/llama3-70b-instruct"
